e6479301cbf8d6f5de6041433cc94dd49aeb2dd8
Yann Gouffon <github@yago-art.ch>
2016-01-20 11:51:49 +0100

    Update 2016-01-20T10:51:47.466Z

--- build/js/main.js
+++ build/js/main.js
@@ -3,3 +3,19 @@
 (function(){
 
 }());
+
+'use strict';
+
+/* global jQuery */
+
+(function($){
+
+  if ($(window).width() < 767) {
+    $(document).ready(function () {
+      $('#search-mobile').click(function () {
+        $('#search-form').toggleClass('open')
+      });
+    });
+  }
+
+}(jQuery));
