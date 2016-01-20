461887e35f2081fdfd4ab8294d8f15adb9aca43e
Yann Gouffon <github@yago-art.ch>
2016-01-20 11:47:18 +0100

    navbar mobile adaptations

---
+++ assets/js/navbar.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* global jQuery */
+
+(function($){
+
+  if ($(window).width() < 767) {
+    $(document).ready(function () {
+      $('#search-mobile').click(function () {
+        $('#search-form').toggleClass('open')
+      });
+    });
+  }
+
+}(jQuery));
