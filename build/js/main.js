cd42e56d6a13ebe78f2464b86df7cd41799a86de
Yann Gouffon <github@yago-art.ch>
2016-02-02 10:49:36 +0100

    Update 2016-02-02T09:49:34.534Z

--- build/js/main.js
+++ build/js/main.js
@@ -57,3 +57,25 @@
   }
 
 }(jQuery));
+
+'use strict';
+
+/* global jQuery */
+
+(function($){
+
+  $(document).ready(function () {
+
+    $('.panel-button').click(function(){
+      console.log('toggle');
+      $(this)
+        .parents('.panel')
+        .toggleClass('panel-enter')
+        .toggleClass('panel-entering')
+        .toggleClass('panel-leave')
+        .toggleClass('panel-leaving');
+    });
+
+  });
+
+}(jQuery));
