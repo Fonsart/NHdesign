963ce236fbf5ea610f3f4da12945e4fbc7b68989
Yann Gouffon <github@yago-art.ch>
2016-02-15 15:16:02 +0100

    Update 2016-02-15T14:15:59.838Z

--- build/js/main.js
+++ build/js/main.js
@@ -22,7 +22,7 @@
   $(document).ready(function () {
     var initGrid = function () {
       $('.media-grid').flexImages({
-        container: '.media-item',
+        container: '.media-item, .group-teaser',
         object: 'img',
         rowHeight: 250,
         truncate: false
