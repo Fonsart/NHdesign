7d48b9eb4ecb4ed488c1b9e425e40e28aed51816
Yann Gouffon <github@yago-art.ch>
2016-03-02 16:33:00 +0100

    add close all facets button - #9

--- assets/js/facets.js
+++ assets/js/facets.js
@@ -10,6 +10,13 @@
       $(this).parents('.facet-group').toggleClass('open');
     });
 
+    $('#close-all-facets').click(function(){
+      console.log('hello');
+      $('.facet-group').each(function(){
+        $(this).removeClass('open');
+      });
+    });
+
   });
 
 }(jQuery));
