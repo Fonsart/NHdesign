1341aa4d3579322509f514047f96c0023c77804e
Yann Gouffon <github@yago-art.ch>
2016-03-02 16:42:47 +0100

    #9 more sexyx close-all button simulation

--- assets/js/facets.js
+++ assets/js/facets.js
@@ -13,7 +13,12 @@
     $('#close-all-facets').click(function(){
       console.log('hello');
       $('.facet-group').each(function(){
-        $(this).removeClass('open');
+        if (!$(this).hasClass('open')) {
+          $('#close-all-facets').html('Tout fermer');
+        } else {
+          $('#close-all-facets').html('Tout ouvrir');
+        }
+        $(this).toggleClass('open');
       });
     });
 
