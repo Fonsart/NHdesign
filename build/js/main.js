094dc8f26661c93065c0d8449fba9c249b47d967
Marc Friederich <marc@antistatique.net>
2016-03-03 17:33:42 +0100

    Update 2016-03-03T16:33:32.691Z

--- build/js/main.js
+++ build/js/main.js
@@ -10,6 +10,34 @@
       $(this).parents('.facet-group').toggleClass('open');
     });
 
+    $('#close-all-facets').click(function(){
+      console.log('hello');
+      $('.facet-group').each(function(){
+        if (!$(this).hasClass('open')) {
+          $('#close-all-facets').html('Tout fermer');
+        } else {
+          $('#close-all-facets').html('Tout ouvrir');
+        }
+        $(this).toggleClass('open');
+      });
+    });
+
+  });
+
+}(jQuery));
+
+'use strict';
+
+/* global jQuery */
+
+(function($){
+
+  $(document).ready(function () {
+
+    $('.inbox-message').click(function(){
+      $('.inbox-message-detail').toggleClass('open');
+    });
+
   });
 
 }(jQuery));
