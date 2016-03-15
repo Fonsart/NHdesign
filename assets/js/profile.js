e20c798b44ef4c97413bba6ed792d30a8b0a47f6
Yann Gouffon <github@yago-art.ch>
2016-03-15 14:55:06 +0100

    nicer collection

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -7,8 +7,8 @@
   $(document).ready(function () {
 
     $('#btn-readmore').click(function(){
-      $('#person-bio').toggleClass('open');
-      if ($('#person-bio').hasClass('open')) {
+      $('#person-bio, #collection-bio').toggleClass('open');
+      if ($('#person-bio, #collection-bio').hasClass('open')) {
         $(this).html('Voir moins');
       } else {
         $(this).html('Lire la suite...');
